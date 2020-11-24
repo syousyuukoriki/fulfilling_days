b6c94bb427456851dcbf88aafe5cb6df9b81b407
syousyuukoriki <5364tarou@gmail.com>
2020-11-24 15:01:12 +0900

    diaryコントローラーの作成

---
+++ app/controllers/diarys_controller.rb
@@ -0,0 +1,2 @@
+class DiarysController < ApplicationController
+end
---
+++ test/controllers/diarys_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DiarysControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
