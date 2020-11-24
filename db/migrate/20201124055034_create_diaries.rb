c4f411438966a8780b396035bb24dd3e82717f67
syousyuukoriki <5364tarou@gmail.com>
2020-11-24 14:57:00 +0900

    diarysテーブルの作成

--- db/migrate/20201124055034_create_diaries.rb
+++ db/migrate/20201124055034_create_diaries.rb
@@ -1,6 +1,10 @@
 class CreateDiaries < ActiveRecord::Migration[6.0]
   def change
     create_table :diaries do |t|
+      t.string   :title,  null: false
+      t.date     :day,    null: false
+      t.integer  :rate,   null: false
+      t.text     :event,  null: false
 
       t.timestamps
     end
